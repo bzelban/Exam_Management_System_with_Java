bd86d7db2c3ea456dadabaded849ce141666bbab
Grim H <bzelban@outlook.com>
2020-04-14 04:38:35 +0300

    Fenrir

--- ExamManagementSystem/src/test.java
+++ ExamManagementSystem/src/test.java
@@ -17,6 +17,8 @@
 
  */
 
+
+
         /*
             Design: USER TABLE
 
@@ -72,11 +74,11 @@
                 LCT: r- w- rw
                 DEV: r- w- rw
 
-            STUDENT ID   /  Midterm 1  /  Midterm 2  /  Final
-            .               .             .             .
-            .               .             .             .
-            .               .             .             .
-            .               .             .             .
+            STUDENT ID   /  LECTURE CODE  /  Midterm 1  /  Midterm 2  /  Final
+            .               .                .             .             .
+            .               .                .             .             .
+            .               .                .             .             .
+            .               .                .             .             .
 
           */
 
@@ -85,17 +87,71 @@
 import java.util.*;
 import java.io.*;
 import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.lang.*;
 
 
+class _user{
+    String ID; String PASSWORD; String TYPE; String CODE; String PWDATE;
+    public _user(String ID, String PASSWORD, String TYPE, String CODE, String PWDATE)
+    {
+        this.ID = ID; this.PASSWORD = PASSWORD; this.TYPE = TYPE; this.CODE = CODE; this.PWDATE = PWDATE;
+    }
+}
+
+class _lecture{
+    String CODE; String NAME; String DATE; String HOUR;
+    public _lecture(String CODE, String NAME, String DATE, String HOUR)
+    {
+        this.CODE = CODE; this.NAME = NAME; this.DATE = DATE; this.HOUR = HOUR;
+    }
+}
+class _result{
+    String ID; String CODE; String MT1; String MT2; String FINAL;
+    public _result(String ID, String CODE, String MT1, String MT2, String FINAL)
+    {
+        this.ID = ID; this.CODE = CODE; this.MT1 = MT1; this.MT2 = MT2; this.FINAL = FINAL;
+    }
+}
 
 
 public class test
 {
 
+    public static ArrayList<_result> _createResultTable()
+    {
+        ArrayList<_result> resultList = new ArrayList<>();
+        resultList.add(new _result("ID", "CODE", "MT1", "MT2", "FINAL"));
+        resultList.add(new _result("3333", "CS50", "80", "", "" ));
+        resultList.add(new _result("333", "SE60", "50", "45", "40"));
 
+        return resultList;
+    }
 
+    public static ArrayList<_lecture> _createLectureTable()
+    {
+        ArrayList<_lecture> lectureList = new ArrayList<>();
+        lectureList.add(new _lecture("CODE", "NAME", "DATE", "HOUR"));
+        lectureList.add(new _lecture("CS50", "COMP SCI", "2020-5-5", "15-00"));
+        lectureList.add(new _lecture("SE60", "NEURO", " ", " "));
+
+        return lectureList;
+        //functor
+    }
+
+    public static ArrayList<_user> _createUserTable()
+    {
+
+        ArrayList<_user> userList = new ArrayList<>();
+        userList.add(new _user("0000", "0000", "AAA", "XXX", "1970-1-1"));
+        userList.add(new _user("ID", "PASSWORD", "TYPE", "CODE", "PWDATE"));
+        userList.add(new _user("1111", "1111", "DEV", "", "2020-1-1"));
+        userList.add(new _user("2222", "2222", "LCT", "", "2020-1-1"));
+        userList.add(new _user("3333", "3333", "STD", "CS50", "2020-1-1"));
+
+        return userList;
+    }
+
+    /*
     public void _login(String ID, String passwd)
     {
         boolean temp = true;
@@ -279,10 +335,15 @@ public class test
 
 
 
-
+    */
     public static void main(String[] args) throws FileNotFoundException {
 
         System.out.println("WELCOME TO EXAM MANAGEMENT SYSTEM");
+
+
+        ArrayList<_user> testList = _createUserTable();
+
+        System.out.println(testList.toString());
 
 
 
