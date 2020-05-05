dcbea407f1a906e038fa72a8790b471714f2fad1
Grim H <bzelban@outlook.com>
2020-05-06 00:44:57 +0300

    JUnit Framework

--- ExamManagementSystem/src/juniterJunitorium.java
+++ ExamManagementSystem/src/juniterJunitorium.java
@@ -1,2 +1,67 @@
-public class juniterJunitorium {
+import static org.junit.jupiter.api.Assertions.*;
+
+class juniterJunitorium {
+
+    @org.junit.jupiter.api.Test
+    void _getUserTable() {
+
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void _getLectureTable() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void _getResultTable() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void _examOverlap() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void _getResults() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void _getDate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void _setExamResults() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void _setExamDate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void _START() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void _login() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void _userTYPE() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void _DEVMENU() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void _STDMENU() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void _LCTMENU() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
 }
