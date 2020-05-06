000ecce2b133fa64c4770b0bb9b78d2ea2df339c
Grim H <bzelban@outlook.com>
2020-05-07 02:55:07 +0300

    Fenrir
    Test partial are Added

--- ExamManagementSystem/src/juniterJunitorium.java
+++ ExamManagementSystem/src/juniterJunitorium.java
@@ -5,50 +5,18 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-
 class juniterJunitorium {
     ArrayList<_user> denemeUser = _user._createUserTable();
     ArrayList<_lecture> denemeLecture = _lecture._createLectureTable();
     ArrayList<_result> denemeResult = _result._createResultTable();
 
+    String str = "";
+
+    //User object (ArrayList Database) POZITIVE is checks as ok
     @org.junit.jupiter.api.Test
-    void _getUserTable() {
+    void _START() { // 1 - 5
 
-
-    }
-
-    @org.junit.jupiter.api.Test
-    void _getLectureTable() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void _getResultTable() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void _examOverlap() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void _getResults() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void _getDate() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void _setExamResults() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void _setExamDate() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void _START_user_pozitive() {
-
-        String str = denemeUser.get(2).getID();
+        str = denemeUser.get(2).getID();
         assertEquals("1111", str);
         // Base User List is not Null and String ID checks ok
 
@@ -69,12 +37,14 @@ class juniterJunitorium {
         // PW_DATE checks from different USR
 
     }
-    @Test
-    void _START_user_negative(){
 
-        String str = denemeUser.get(11).getPASSWORD();
-         assertEquals("1111", str);
-         // Base User List is not Null and String. Also Password checks are ok
+    /*
+    //User object NEGATIVE is checks as OK
+    void _START() { // 2 - 5
+
+        str = denemeUser.get(11).getPASSWORD();
+        assertEquals("1111", str);
+        // Base User List is not Null and String. Also Password checks are ok
 
         str = denemeUser.get(11).getID();
         assertEquals("aaa", str);
@@ -83,39 +53,149 @@ class juniterJunitorium {
         str = denemeUser.get(11).getTYPE();
         assertEquals("bbb", str);
         // TYPE checks are ok
+
+        str = denemeUser.get(11).getCODE();
+        assertEquals("lılıl", str);
+        // CODE checks are ok
+
+        str = denemeUser.get(11).getPW_DATE();
+        assertEquals("bbbb", str);
+        // PW_DATE checks are ok
     }
 
+    //Lecture Object checks ok as pozitive
     @Test
-    void _START_lecture_pozitive(){
+    void _START() { // 3 - 1
 
-        String str = denemeLecture.get(2).getCODE();
+        str = denemeLecture.get(2).getCODE();
         assertEquals("SE60", str);
         // Base Lecture List is not Null and String. Also CODE checks are ok
     }
 
+    //Lecture object checks ok as negative
     @Test
-    void _START_lecture_negative(){
-        String str = denemeLecture.get(5).getCODE();
+    void _START() { // 4 - 1
+        str = denemeLecture.get(5).getCODE();
         assertEquals("SE221", str);
         // Base Lecture List is not Null and String. Also Code checks are ok
     }
 
+    //Result object checks ok as pozitive
     @Test
-    void _START_result_pozitive(){
-        String str = denemeResult.get(2).getFINAL();
+    void _START() { // 5 - 1
+        str = denemeResult.get(2).getFINAL();
         assertEquals("40", str);
         // Base Result List is not Null and String. Also FINAL checks are ok
     }
 
+    //Result object checks ok as negative
     @Test
-    void _START_result_negative(){
-        String str = denemeResult.get(2).getMT1();
+    void _START() { // 6 - 2
+        str = denemeResult.get(2).getMT1();
         assertEquals("50", str);
         // Base Result List is not Null and String. Also MT1 checks are ok
 
         str = denemeResult.get(2).getMT2();
         assertEquals("45", str);
+
     }
+    */
+
+    @org.junit.jupiter.api.Test
+    void _getUserTable() { // 7 - 1
+
+        str = denemeUser.get(5).getID();
+        assertEquals("4444", str);
+        // UserTable checks in _getUserTable as pozitive
+
+    }
+
+    /*
+    @org.junit.jupiter.api.Test
+    void _getUserTable() { // 8 - 1
+        str = denemeUser.get(5).getID();
+        assertEquals("3131", str);
+        //UserTable checks in _getUserTable as negative
+    }
+    */
+
+    @org.junit.jupiter.api.Test
+    void _getLectureTable() { // 8 - 1
+        str = denemeLecture.get(7).getCODE();
+        assertEquals("SE221", str);
+        //LectureTable checks in _getLectureTable() as Pozitive
+
+
+    }
+
+    /*
+    @org.junit.jupiter.api.Test
+    void _getLectureTable() // 9 - 1
+    {
+        str = denemeLecture.get(7).getCODE();
+        assertEquals("MATH153", str);
+        //LectureTable checks in_getLectureTable() as Negative
+    }
+    */
+
+    @org.junit.jupiter.api.Test
+    void _getResultTable() { // 10 - 1
+
+        str = denemeResult.get(2).getFINAL();
+        assertEquals("40", str);
+        //ResulTable checks in _getResultTable() as Pozitive
+    }
+
+    /*
+    @Test
+    void _getResultTable() { // 11 - 1
+
+        str = denemeResult.get(2).getFINAL();
+        assertEquals("31", str);
+        //ResultTable checks in _getResulTable() as Negative
+
+    }
+    */
+
+    @org.junit.jupiter.api.Test
+    void _examOverlap() { // 12 - 2
+
+        str = denemeLecture.get(3).getDATE();
+        assertEquals("2020-6-1", str);
+
+        str = denemeLecture.get(3).getHOUR();
+        assertEquals("16.00", str);
+    }
+
+    /*
+    @Test
+    void _examOverlap(){ // 13 - 2
+
+        str = denemeLecture.get(3).getDATE();
+        assertEquals("",str);
+
+        str = denemeLecture.get(3).getDATE();
+        assertEquals("",str);
+
+    }
+    */
+
+    @org.junit.jupiter.api.Test
+    void _getResults() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void _getDate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void _setExamResults() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void _setExamDate() {
+    }
+
 
     @org.junit.jupiter.api.Test
     void _login() {
